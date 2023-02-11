3b3124774fbfb072a8a9f30009465f893f361763
Thomson <tomsinonyedikachi@gmail.com>
2023-02-11 06:40:36 -0800

    Navbar transparent

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,47 +1,88 @@
 import React, { useEffect, useState } from "react";
 import Logo from "../images/logo-white.svg";
+import Logo2 from "../images/logo.svg";
 import { FaAngleDown } from "react-icons/fa";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-  
   const [open, setOpen] = useState(false);
-  const [scroll, setScroll] = useState(0);
+  const [scroll, setScroll] = useState(false);
 
   useEffect(() => {
     const handleScoll = () => {
-      setScroll(window.pageYOffset);
+      if (window.scrollY > 90) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
     };
     window.addEventListener("scroll", handleScoll);
   }, []);
 
-  return ( 
-    <nav className= {`flex justify-around items-center py-7 sticky top-0  bg-LightBlue shadow-md`}>
+  return (
+    <nav
+      className={`flex justify-around items-center py-5 sticky top-0
+       ${ scroll ? "bg-white bg-opacity-90 shadow-md" : "bg-LightBlue"
+     }  `}
+
+    >
       <div>
-        <img src={Logo} alt="logo" className="h-17  cursor-pointer" />
+        {scroll ? (
+          <img src={Logo2} alt="logo" className="h-17  cursor-pointer" />
+        ) : (
+          <img src={Logo} alt="logo" className="h-17  cursor-pointer" />
+        )}
       </div>
 
-
-
       <div
-          onClick={() => setOpen(!open)}
-          className="text-3xl absolute right-4 top-6 cursor-pointer md:hidden"
-        >
-          {open ? <FaTimes className="text-white" /> : <FaBars className="text-white" />}
-        </div>
+        onClick={() => setOpen(!open)}
+        className="text-3xl absolute right-4 top-6 cursor-pointer md:hidden"
+      >
+        {open ? (
+          <FaTimes className={scroll ? "text-black" : "text-white"} />
+        ) : (
+          <FaBars className={scroll ? "text-black" : "text-white"} />
+        )}
+      </div>
       <div>
-
-
-        <ul className={`absolute  w-full p-12 md:p-0 space-y-10 md:space-y-0 
-        bg-LightBlue font-bold  right-0 top-[85px] md:top-0 
-        text-white md:space-x-8 transition-all ease-in  text-bg md:flex  md:static ${open ? 'left-0': 'left-[-100%]'}`}  >
+        <ul
+          className={` hidden   md:p-0 space-y-10 md:space-y-0 
+       font-bold  right-0  first-line:top-[85px] md:top-0
+       ${scroll ? "text-black" : "text-white"}   md:space-x-8 
+          text-bg md:flex  
+        md:static ${open ? "left-0" : "left-[-100%]"}
+          }
+        `}
+        >
           <li>
-            <a href="#">Developers</a>
+            <a className="" href="#">
+              Developers
+            </a>
           </li>
           <li>
             <a href="#">Princing</a>
           </li>
-          <div className="flex items-center space-x-1">
+          <div className="flex items-center sm:justify-between space-x-1">
+            <li>
+              <a href="#">Learn</a>
+            </li>
+            <FaAngleDown />
+          </div>
+        </ul>
+
+        {/* Mobile */}
+
+        <ul className=  {`md:hidden absolute top-20 text-xl
+        left-0 mx-5 w-[90%] transition-all ease-in ${scroll ? 'bg-slate-100' : ' bg-slate-100'}  ${open ? 'left-0' : ' left-[-100%]'} space-y-8  bg-white p-12`}  >
+        <li>
+            <a className="" href="#">
+              Developers
+            </a>
+          </li>
+          <li>
+            <a href="#">Princing</a>
+          </li>
+          <div className="flex items-center sm:justify-between space-x-1">
             <li>
               <a href="#">Learn</a>
             </li>
@@ -63,4 +104,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
- 
